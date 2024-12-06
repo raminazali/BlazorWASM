9684f7d8f86b7efd49020b961df29cfb03376598
Ramin Azali <53081527+raminazali@users.noreply.github.com>
2024-12-06 23:04:58 +0330

    website first page is ok now

---
+++ BlazorWASM/wwwroot/js/plugins.init.js
@@ -0,0 +1,185 @@
+/* Template Name: Dashit - Saas & Software Multipurpose Template
+   Author: Shreethemes
+   Email: support@shreethemes.in
+   Website: https://shreethemes.in
+   Version: 1.0.0
+   Created: July 2024
+   File Description: Common JS file of the template(plugins.init.js)
+*/
+
+
+/***********************************/
+/*         INDEX                   */
+/*==================================
+ *     01.  Tiny Slider            *
+ *     02.  Countdown Js           * (For Comingsoon pages)
+ *     03.  Maintenance js         * (For Maintenance page)
+ *     04.  Tobii lightbox         * (For Portfolio pages)
+ *     05.  Data Counter           *
+ *     06.  Typed Text animation   *
+ ==================================*/
+         
+//=========================================//
+/*            01) Tiny slider              */
+//=========================================//
+if(document.getElementsByClassName('tiny-single-item').length > 0) {
+    var slider = tns({
+        container: '.tiny-single-item',
+        items: 1,
+        controls: false,
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        speed: 400,
+        gutter: 0,
+    });
+};
+
+if(document.getElementsByClassName('tiny-three-item').length > 0) {
+    var slider = tns({
+        container: '.tiny-three-item',
+        controls: false,
+        mouseDrag: true,
+        loop: true,
+        rewind: true,
+        autoplay: true,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 3000,
+        navPosition: "bottom",
+        speed: 400,
+        gutter: 12,
+        responsive: {
+            992: {
+                items: 3
+            },
+
+            767: {
+                items: 2
+            },
+
+            320: {
+                items: 1
+            },
+        },
+    });
+};
+//=========================================//
+/*/*            02) Countdown js           */
+//=========================================//
+
+try {
+    if(document.getElementById("days")){
+        // The data/time we want to countdown to
+        var eventCountDown = new Date("December 25, 2025 16:37:52").getTime();
+
+        // Run myfunc every second
+        var myfunc = setInterval(function () {
+
+            var now = new Date().getTime();
+            var timeleft = eventCountDown - now;
+
+            // Calculating the days, hours, minutes and seconds left
+            var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+            // Result is output to the specific element
+            document.getElementById("days").innerHTML = days + "<p class='count-head'>Days</p> "
+            document.getElementById("hours").innerHTML = hours + "<p class='count-head'>Hours</p> "
+            document.getElementById("mins").innerHTML = minutes + "<p class='count-head'>Mins</p> "
+            document.getElementById("secs").innerHTML = seconds + "<p class='count-head'>Secs</p> "
+
+            // Display the message when countdown is over
+            if (timeleft < 0) {
+                clearInterval(myfunc);
+                document.getElementById("days").innerHTML = ""
+                document.getElementById("hours").innerHTML = ""
+                document.getElementById("mins").innerHTML = ""
+                document.getElementById("secs").innerHTML = ""
+                document.getElementById("end").innerHTML = "00:00:00:00";
+            }
+        }, 1000);
+    }
+} catch (err) {
+
+}
+
+
+//=========================================//
+/*/*            03) Maintenance js         */
+//=========================================//
+
+try {
+    if(document.getElementById("maintenance")){
+        var seconds = 3599;
+        function secondPassed() {
+            var minutes = Math.round((seconds - 30) / 60);
+            var remainingSeconds = seconds % 60;
+            if (remainingSeconds < 10) {
+                remainingSeconds = "0" + remainingSeconds;
+            }
+            document.getElementById('maintenance').innerHTML = minutes + ":" + remainingSeconds;
+            if (seconds == 0) {
+                clearInterval(countdownTimer);
+                document.getElementById('maintenance').innerHTML = "Buzz Buzz";
+            } else {
+                seconds--;
+            }
+        }
+        var countdownTimer = setInterval('secondPassed()', 1000);
+    }
+} catch (err) {
+
+}
+
+//=========================================//
+/*/*            04) Tobii lightbox         */
+//=========================================//
+
+try {
+    const tobii = new Tobii()
+} catch (err) {
+
+}
+
+//=========================================//
+/*/*            05) Data Counter           */
+//=========================================//
+
+try {
+    const counter = document.querySelectorAll('.counter-value');
+    const speed = 2500; // The lower the slower
+
+    counter.forEach(counter_value => {
+        const updateCount = () => {
+            const target = +counter_value.getAttribute('data-target');
+            const count = +counter_value.innerText;
+
+            // Lower inc to slow and higher to slow
+            var inc = target / speed;
+
+            if (inc < 1) {
+                inc = 1;
+            }
+
+            // Check if target is reached
+            if (count < target) {
+                // Add inc to count and output in counter_value
+                counter_value.innerText = (count + inc).toFixed(0);
+                // Call function every ms
+                setTimeout(updateCount, 1);
+            } else {
+                counter_value.innerText = target;
+            }
+        };
+
+        updateCount();
+    });
+} catch (err) {
+
+}
